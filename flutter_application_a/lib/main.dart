e97402e0633406612696952a6c6bd3f63ce60fdf
muguk04-4 <garuru7080@gmail.com>
2023-12-02 16:27:35 +0900

    basic structure implement

--- flutter_application_a/lib/main.dart
+++ flutter_application_a/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(MyApp());
 }
 
+// *********** StatefulWidget 으로 바꿔야함 **********
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -13,9 +14,46 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 3,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Elevated BUTTON"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Elevated BUTTON"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Elevated BUTTON"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Elevated BUTTON"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Elevated BUTTON"),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(shape: CircleBorder()),
+              onPressed: () {},
+              child: Text(
+                //*********** 나중에 값으로 변경 ******/
+                "1:00",
+                style: TextStyle(fontSize: 30.0),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-  // go here
-  //https://api.flutter.dev/flutter/widgets/GridView-class.html
 }
