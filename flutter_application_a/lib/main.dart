b3e87bc3b6c0588b8c7ce87e48f8952c78ba0f60
muguk04-4 <garuru7080@gmail.com>
2023-12-10 20:05:09 +0900

    pause process not working. longPress function need.

--- flutter_application_a/lib/main.dart
+++ flutter_application_a/lib/main.dart
@@ -1,64 +1,100 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _Ott();
-}
-
-// *********** StatefulWidget 으로 바꿔야함 **********
-class _Ott extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(),
-        body: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 3,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Elevated BUTTON"),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Elevated BUTTON"),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Elevated BUTTON"),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Elevated BUTTON"),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Elevated BUTTON"),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(shape: CircleBorder()),
-              onPressed: () {},
-              child: Text(
-                //*********** 나중에 값으로 변경 ******/
-                "1:00",
-                style: TextStyle(fontSize: 30.0),
-                textAlign: TextAlign.center,
+      home: OneTouchTimerApp(),
+    );
+  }
+}
+
+class OneTouchTimerApp extends StatefulWidget {
+  @override
+  _OneTouchTimerAppState createState() => _OneTouchTimerAppState();
+}
+
+class _OneTouchTimerAppState extends State<OneTouchTimerApp> {
+  List<int> timerDurations = [10, 20, 30, 40, 50, 60];
+  List<int> originalDurations = [];
+  List<Timer?> timers = List.filled(6, null);
+
+  @override
+  void initState() {
+    super.initState();
+    originalDurations = List.from(timerDurations);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('OneTouchTimer'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 15.0, // 버튼 사이의 가로 여백
+            mainAxisSpacing: 15.0, // 버튼 사이의 세로 여백
+          ),
+          itemBuilder: (context, index) {
+            return ElevatedButton(
+              onPressed: () {
+                _startStopTimer(index);
+              },
+              style: ElevatedButton.styleFrom(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100.0),
+                ),
               ),
-            ),
-          ],
+              child: Center(
+                child: Text(
+                  '${timerDurations[index]}s',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            );
+          },
+          itemCount: timerDurations.length,
         ),
       ),
     );
+  }
+
+  void _startStopTimer(int buttonIndex) {
+    if (timers[buttonIndex] == null) {
+      timers[buttonIndex] = Timer.periodic(Duration(seconds: 1), (timer) {
+        if (timerDurations[buttonIndex] > 0) {
+          setState(() {
+            timerDurations[buttonIndex]--;
+          });
+        } else {
+          _stopTimer(buttonIndex);
+        }
+      });
+    } else {
+      _resetTimer(buttonIndex);
+    }
+  }
+
+  void _resetTimer(int buttonIndex) {
+    timers[buttonIndex]?.cancel();
+    timers[buttonIndex] = null;
+    setState(() {
+      timerDurations[buttonIndex] = originalDurations[buttonIndex];
+    });
+  }
+
+  void _stopTimer(int buttonIndex) {
+    timers[buttonIndex]?.cancel();
+    timers[buttonIndex] = null;
   }
 }
