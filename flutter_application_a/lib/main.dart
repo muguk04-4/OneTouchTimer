6f3c9c3167f4e28bb1eb77bf359895da32396d56
muguk04-4 <garuru7080@gmail.com>
2024-01-13 16:26:50 +0900

    add comments. have to add vibrate function when it alarm.

--- flutter_application_a/lib/main.dart
+++ flutter_application_a/lib/main.dart
@@ -20,10 +20,11 @@ class OneTouchTimerApp extends StatefulWidget {
 }
 
 class _OneTouchTimerAppState extends State<OneTouchTimerApp> {
+  // 타이머 초기 설정 값
   List<int> timerDurations = [30, 60, 90, 120, 300, 600];
   List<int> originalDurations = [];
   List<Timer?> timers = List.filled(6, null);
-  // 버튼 색을 저장하는 리스트
+  // 각 버튼의 배경 색을 저장하는 리스트
   List<Color> buttonColors = List.filled(6, Colors.blue);
 
   @override
@@ -49,9 +50,11 @@ class _OneTouchTimerAppState extends State<OneTouchTimerApp> {
           itemBuilder: (context, index) {
             return ElevatedButton(
               onPressed: () {
+                // 버튼의 현재 상태에 따라 타이머 시작 또는 정지
                 _startStopTimer(index);
               },
               onLongPress: () {
+                // 버튼을 길게 누르면 타이머 초기화
                 _resetTimer(index);
               },
               style: ElevatedButton.styleFrom(
@@ -63,7 +66,7 @@ class _OneTouchTimerAppState extends State<OneTouchTimerApp> {
               ),
               child: Center(
                 child: Text(
-                  '${timerDurations[index]}s',
+                  '${timerDurations[index]}초',
                   style: TextStyle(fontSize: 18),
                 ),
               ),
@@ -77,12 +80,15 @@ class _OneTouchTimerAppState extends State<OneTouchTimerApp> {
 
   void _startStopTimer(int buttonIndex) {
     if (timers[buttonIndex] == null) {
+      // 타이머가 실행 중이 아니라면 시작
       timers[buttonIndex] = Timer.periodic(Duration(seconds: 1), (timer) {
         if (timerDurations[buttonIndex] > 0) {
+          // 매 초마다 타이머 시간 감소
           setState(() {
             timerDurations[buttonIndex]--;
           });
         } else {
+          // 타이머가 0이 되면 정지하고 버튼 색 변경
           setState(() {
             timers[buttonIndex]?.cancel();
             timers[buttonIndex] = null;
@@ -91,21 +97,23 @@ class _OneTouchTimerAppState extends State<OneTouchTimerApp> {
         }
       });
     } else {
+      // 타이머가 실행 중이라면 정지
       _stopTimer(buttonIndex);
     }
   }
 
   void _resetTimer(int buttonIndex) {
+    // 타이머를 정지하고 초기 설정 값으로 초기화, 버튼 색 초기화
     timers[buttonIndex]?.cancel();
     timers[buttonIndex] = null;
     setState(() {
       timerDurations[buttonIndex] = originalDurations[buttonIndex];
-      // 버튼의 배경 색을 초기 상태로 돌아가게 만듦
-      buttonColors[buttonIndex] = Colors.blue; // 예시로 초기 색상을 파란색으로 설정
+      buttonColors[buttonIndex] = Colors.blue; // 예시: 파란색으로 초기화
     });
   }
 
   void _stopTimer(int buttonIndex) {
+    // 타이머를 정지
     timers[buttonIndex]?.cancel();
     timers[buttonIndex] = null;
   }
