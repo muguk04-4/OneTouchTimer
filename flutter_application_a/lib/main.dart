c5df7b273fe1a15e0572610212bb4498e1b35eb5
muguk04-4 <garuru7080@gmail.com>
2024-03-21 21:13:37 +0900

    need more dart knowledge

--- flutter_application_a/lib/main.dart
+++ flutter_application_a/lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 import 'package:vibration/vibration.dart';
 
-// import 'TimerAddPage.dart';
+// 타이머 데이터 관리용
 import 'TimerService.dart';
 
 void main() {
@@ -26,9 +27,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // 아래 방법 중 하나 채택해야됨
-  // 1. 값 입력 int -> duration -> timer()로 타이머 반영
-  // 2. 값 입력 duration -> timer()로 타이머 반영
+// 타이머 시간을 int로 받고 duration으로 전환 -> main으로 돌아와timer()로 타이머 반영
   List<int> timerDurations = [5, 10, 30, 60];
   //초기값을 저장해 _resetTimer에 사용
   List<int> originalDurations = [];
@@ -248,5 +247,82 @@ class _MyAppState extends State<MyApp> {
     String seconds = twoDigits(duration.inSeconds.remainder(60));
 
     return "$hours:$minutes:$seconds";
+  }
+}
+
+// 타이머 생성 페이지
+// 타이머 용으로 바꿔야됨!!!!!!!!!!!!!!!!!!!!!!!!!!
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  // TextField의 값을 가져올 때 사용합니다.
+  TextEditingController textController = TextEditingController();
+
+  // 경고 메세지
+  String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("타이머 추가"),
+        // 뒤로가기 버튼
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // 텍스트 입력창
+            TextField(
+              controller: textController, // 연결해 줍니다.
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "타이머 입력",
+                errorText: error,
+              ),
+            ),
+            SizedBox(height: 32),
+            // 추가하기 버튼
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                child: Text(
+                  "추가하기",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  // 추가하기 버튼 클릭시
+                  String job = textController.text; // 값 가져오기
+                  if (job.isEmpty) {
+                    setState(() {
+                      error = "시, 분, 초를 입력해주세요."; // 내용이 없는 경우 에러 메세지
+                    });
+                  } else {
+                    setState(() {
+                      error = null; // 내용이 있는 경우 에러 메세지 숨기기
+                    });
+                    Navigator.pop(context, job); // job 변수를 반환하며 화면을 종료합니다.
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
